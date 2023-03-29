0f48cc7ed7c045f65343b7e6846a8cbb747396ab
Laura Alayón Domíguez <91117925+lalayond@users.noreply.github.com>
2023-03-29 16:00:33 +0100

    Update tarea1.sql

--- tareas/programación/tarea1.sql
+++ tareas/programación/tarea1.sql
@@ -38,27 +38,32 @@ DELIMITER //
 CREATE PROCEDURE obtener_datos
 BEGIN
     SELECT c.*, p.* FROM cientifico as c join cientifico_proyecto as cp on cp.ref_cientifico=c.id join proyecto as p on p.id=cp.ref_proyecto;
-END //
+END
+//
 
 DELIMITER //
 CREATE PROCEDURE obtener_numero_proyectos
 BEGIN 
     SELECT count(ref_proyecto) FROM cientifico_proyecto GROUP BY ref_cientifico;
-END //
+END 
+//
 
 DELIMITER //
 CREATE PROCEDURE obtener_numero_cientificos
 BEGIN
     SELECT cp.ref_proyecto, p.nombre, count(cp.ref_cientifico),cp.ref_proyecto, p.nombre FROM proyecto as p join cientifico_proyecto as cp on cp.ref_proyecto=p.id GROUP BY cp.ref_proyecto;
-END//
-DELIMITER $$ -- comienzo del delimitador
+END
+//
+DELIMITER //
 CREATE PROCEDURE obtener_horas_cientificos
 BEGIN
     SELECT c.id, c.nombre, p.horas from proyecto as p join cientifico_proyecto as cp on p.id=cp.ref_proyecto join cientifico as c on c.id=cp.ref_cientifico group by ref_cientifico;
-END//
+END
+//
 
 DELIMITER //
 CREATE PROCEDURE obtener_dni_nombre_cientificos(in horas_min int)
 BEGIN
     SELECT c.* FROM cientifico as c join cientifico_proyecto as cp on c.id=cp.ref_cientifico join proyecto as p on cp.ref_proyecto=p.id group by cp.ref_cientifico having count(ref_proyecto) > 1 and sum(cp.horas) > horas_min;
-END//
+END 
+//
