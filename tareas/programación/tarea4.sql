319b863e35aa72b2b6678c189deddeeced72a91f
Laura Alayón Domíguez <91117925+lalayond@users.noreply.github.com>
2023-04-14 15:15:58 +0100

    Update tarea4.sql

--- tareas/programación/tarea4.sql
+++ tareas/programación/tarea4.sql
@@ -1,9 +1,9 @@
---- Creación de la base de datos
+--- Creación de la base de datos --
 mysql -u root -p 
 mysql> create database gestion_clientes;
 mysql> use gestion_clientes;
 
---- Creación de la table personas
+--- Creación de la table personas --
 create table persona(
     id  VARCHAR(50) primary key,
     nombre VARCHAR(50) ,
@@ -13,7 +13,7 @@ create table persona(
     sexo ENUM('H', 'M')
 );
 
---- Realizar al menos 10 insert en la tabla
+--Realizar insert en la tabla--
 insert into persona values('11111111A', 'Pepe', 'Perez', 'Hernández', 81, 'H');
 insert into persona values('22222222A', 'Juan', 'Hernández', 'Perez', 50, 'H');
 insert into persona values('33333333A', 'Maria', 'Domínguez', 'Perez', 95, 'M');
@@ -25,8 +25,8 @@ insert into persona values('88888888B', 'Pedro', 'Rodriguez', 'Ramos', 73, 'H');
 insert into persona values('99999999C', 'Lauranio', 'Perez', 'Hernández', 65, 'H');
 insert into persona values('10101010C', 'Paula', 'Alonso', 'Fuentes', 86, 'M');
 
---- Creación de procedimientos 
---- 1. Insertar información en la tabla "personas"
+--- Creación de procedimientos --
+--- 1. Insertar información en la tabla "personas" --
 DELIMITER //
 DROP PROCEDURE IF EXISTS info_personas //
 CREATE PROCEDURE info_personas(in nuevo_id VARCHAR(50), in nuevo_nombre VARCHAR(50), in nuevo_apellido1 VARCHAR(50), in nuevo_apellido2 VARCHAR(50), in nuevo_peso int, in nuevo_sexo VARCHAR(50))
@@ -35,7 +35,7 @@ BEGIN
 END 
 //
 
---- 2. Actualizar el nombre e id de un cliente
+--- 2. Actualizar el nombre e id de un cliente --
 DELIMITER //
 DROP TABLE IF EXISTS actualizar_nombre //
 CREATE PROCEDURE actualizar_nombre(in nuevo_id VARCHAR(50), in nuevo_nombre VARCHAR(50))
@@ -45,7 +45,7 @@ BEGIN
 END
 //
 
---- 3. Eliminar un cliente
+--- 3. Eliminar un cliente --
 DELIMITER //
 DROP PROCEDURE IF EXISTS eliminar_cliente //
 CREATE PROCEDURE eliminar_cliente(in id_eliminar VARCHAR(50))
@@ -54,10 +54,10 @@ BEGIN
 END
 //
 
---- 4. Investigar procedimientos con paramentaros de salida.
+--- 4. Investigar procedimientos con paramentaros de salida. --
 DELIMITER //
 DROP PROCEDURE IF EXISTS obtener_peso //
-CREATE PROCEDURE obtener_peso(in nombre_persona varchar(50), out peso_obtenido int)
+CREATE PROCEDURE obtener_peso(in nombre_persona varchar(50), out peso_obtenido)
 BEGIN
     set peso_obtenido = (
         select peso from persona 
@@ -67,17 +67,17 @@ END
 //
 
 
---- 5. Investigar cómo hacer un ciclo (while).
+--- 5. Investigar cómo hacer un ciclo (while). --
 DELIMITER // 
 DROP PROCEDURE IF EXISTS insertar_personas //
-CREATE PROCEDURE insertar_personas( in numero_de_personas int)
+CREATE PROCEDURE insertar_personas( in numero_de_personas)
 BEGIN
-    DECLARE clave varchar(9); /* Todos los declare tienen que ir juntos, porque sino da error */
+    DECLARE clave varchar(9); 
     DECLARE numero INT;
     SET numero = 1;
     while numero < numero_de_personas do
         SET clave = CONCAT('000000', numero);
-        insert into persona values(clave, 'Juan', 'Ramos', 'Hernández', 65, 'H');
+        insert into persona values(clave, 'Pepe', 'Perez', 'Hernández', 81, 'H');
         SET numero = numero +1;
     end while;
 END
